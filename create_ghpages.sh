8691ffce8030712db80f7bbc279ae1ed190eb1a1
joelkim <kim.dohhyoung@gmail.com>
2018-01-08 14:08:26 +0900

    create_ghpages 수정

--- create_ghpages.sh
+++ create_ghpages.sh
@@ -7,16 +7,23 @@ git push origin docs-korean
 
 git checkout -B gh-pages
 git rebase docs-korean
+
 touch .nojekyll
+
 echo '!build/' >> .gitignore
+echo '!docs/source/commands/' >> .gitignore
+
 source activate conda-docs
+
 cd docs
 make html
 cd ..
+
 git add . -A
 git commit -m "build"
 git push -f origin gh-pages
 git reset --hard HEAD
 git clean -f
 git checkout docs-korean
+
 source deactivate
